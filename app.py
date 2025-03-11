62b99dc867bf039176973a2c57d03942a1baefe6
Jude <judealberca4@gmail.com>
2025-03-11 11:07:01 +0800

    idsad

--- app.py
+++ app.py
@@ -98,6 +98,6 @@ def get_recommendations(user_id):
     
     return jsonify(recommendations), 200
 
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
 
+if __name__ == "__main__":
+    app.run(debug=True)
