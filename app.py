a25253cb627483c2897a8ddc7a1c24e1ec9811d9
Jude <judealberca4@gmail.com>
2025-03-11 10:58:13 +0800

    dsa

--- app.py
+++ app.py
@@ -1,28 +1,14 @@
-import os
 import json
 from flask import Flask, request, jsonify
 import firebase_admin
 from firebase_admin import credentials, db
 from collections import defaultdict
-
-# Fetch Firebase credentials from environment variables
-firebase_credentials = {
-    "type": os.getenv("FIREBASE_TYPE"),
-    "project_id": os.getenv("FIREBASE_PROJECT_ID"),
-    "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
-    "private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace('\\n', '\n'),  # Ensure \n is replaced with an actual newline
-    "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
-    "client_id": os.getenv("FIREBASE_CLIENT_ID"),
-    "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
-    "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
-    "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_X509_CERT_URL"),
-    "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_X509_CERT_URL")
-}
+from credentials import firebase_credentials
 
 # Initialize Firebase with the service account key
 cred = credentials.Certificate(firebase_credentials)
 firebase_admin.initialize_app(cred, {
-    'databaseURL': os.getenv('FIREBASE_DATABASE_URL')  # Fetch Firebase DB URL from environment
+    'databaseURL': "https://gearup-df833-default-rtdb.asia-southeast1.firebasedatabase.app/"  # Use your Firebase Realtime Database URL
 })
 
 app = Flask(__name__)
